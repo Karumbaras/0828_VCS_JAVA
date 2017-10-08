198f5ac22bfe6a915d13bfc32a3854f200e2877a
Geologas <manvydas.saltis@gmail.com>
2017-10-08 16:23:41 +0300

    komiCIUX

--- LaivuMusis/src/lt/vcs/laivumusis/piratai/MockZaidimas.java
+++ LaivuMusis/src/lt/vcs/laivumusis/piratai/MockZaidimas.java
@@ -133,10 +133,6 @@ public class MockZaidimas implements Zaidimas {
 	@Override
 	public ZaidimoLenta duokZaidimoLenta(String zaidejoId) {
 		
-		
-		
-		
-		
 		//atiduoti lentos kopija zaidejui
 		return new MockZaidimoLenta(lentosIlgis,lentosPlotis);
 	}
