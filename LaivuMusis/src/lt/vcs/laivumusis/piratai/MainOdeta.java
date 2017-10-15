76c25673c4212ef3d7688d002418b125d2e87a49
Ooode <odeta.valickaite@gmail.com>
2017-10-15 22:03:04 +0300

    Pakoreguoti MainOdeta ir ZaidejasOdeta

--- LaivuMusis/src/lt/vcs/laivumusis/piratai/MainOdeta.java
+++ LaivuMusis/src/lt/vcs/laivumusis/piratai/MainOdeta.java
@@ -1,10 +1,39 @@
 package lt.vcs.laivumusis.piratai;
 
+import lt.vcs.laivumusis.piratai.zaidejas.Zaidejas;
+
 public class MainOdeta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Zaidimas zaidimasOdetos = new Zaidimas();
+		
+		//ZaidimoLenta zaidimoLentaOdetos = new ZaidimoLenta(10, 10);
+		
+		Zaidejas zaidejasOdetosPirmas = new Zaidejas(zaidimasOdetos);
+		Zaidejas zaidejasOdetosAntras = new Zaidejas(zaidimasOdetos);
 
+		zaidejasOdetosPirmas.getZaidimas();
+		
+		zaidimasOdetos.registruokZaideja();
+		
+		int [][] laivai = {{1,2}};
+		
+		//zaidejasOdetosPirmas.getZaidimas().duokLaivus(zaidejoId);
+		
+		
+		//Thread zaidejasOdetosPirmas = new Thread(new Zaidejas(zaidimasOdetos));
+		//Thread zaidejasOdetosAntras = new Thread(new Zaidejas(zaidimasOdetos));
+
+		//zaidejasOdetosPirmas.start();
+		//zaidejasOdetosAntras.start();
+	
+		
+		//Vaizdas vaizdasOdetos = new Vaizdas(zaidimoLentaOdetos);
+		
+		//vaizdasOdetos.pieskVaizda();
+		
+		
 	}
 
 }
--- LaivuMusis/src/lt/vcs/laivumusis/piratai/zaidejas/ZaidejasOdeta.java
+++ LaivuMusis/src/lt/vcs/laivumusis/piratai/zaidejas/ZaidejasOdeta.java
@@ -17,36 +17,66 @@ public class ZaidejasOdeta implements lt.vcs.laivumusis.common.Zaidejas {
 		public ZaidejasOdeta(Zaidimas zaidimas) {
 			this.zaidimas = zaidimas;
 		}
+		
+		@Override
+		public Zaidimas getZaidimas() {
+			return this.zaidimas;
+		}
 
 		@Override
 		public void run() {
+			
 			this.zaidejoId = zaidimas.registruokZaideja();
-			System.out.println(this.zaidejoId);
+			System.out.println("Sveiki, Jus prisiregistravote. Jusu zaidejo ID yra: " + this.zaidejoId);
 			try {
 				while (true) {
 					
-					Thread.sleep(1000);
+					Thread.sleep(2000);
 					Busena zaidimoBusena = zaidimas.tikrinkBusena(zaidejoId);
 					System.out.println(zaidimoBusena);
+					Thread.sleep(2000);
+					
 					if (zaidimoBusena == Busena.DalinamesZemelapius) {
 						ZaidimoLenta zaidimoLenta = zaidimas.duokZaidimoLenta(zaidejoId);
-						Thread.sleep(1000);
+						Thread.sleep(2000);
 					}
 
 					if (zaidimoBusena == Busena.DalinemesLaivus) {
 						List<Laivas> laivuListas = zaidimas.duokLaivus(zaidejoId);
-			
-						List<Langelis> langeliai = new ArrayList<Langelis>();
-						for (int i = 0; i < laivuListas.get(0).getLaivoIlgis(); i++) {
+						Thread.sleep(2000);
+						
+						/*for (int i = 0; i < laivuListas.get(0).getLaivoIlgis(); i++) {
 							langeliai.add(new lt.vcs.laivumusis.piratai.Langelis("A", i+1));
 						}
 		
 						laivuListas.get(0).setKordinates(langeliai);
 
-						zaidimas.pridekLaiva(laivuListas.get(0), this.zaidejoId);
-						//}
+						zaidimas.pridekLaiva(laivuListas.get(0), this.zaidejoId);*/
+						
 					}
 
+					if(zaidimoBusena == Busena.RikiuojamLaivus) {
+						// 
+							
+						}
+						
+					if (zaidimoBusena == Busena.TavoEile) {
+						
+					}
+					
+					if (zaidimoBusena == Busena.PriesininkoEile) {
+						
+					}
+					
+					if (zaidimoBusena == Busena.TuLaimejai) {
+						System.out.println("Sveikiname, tu laimejai zaidima!");
+						
+					}
+					
+					if (zaidimoBusena == Busena.PriesasLaimejo) {
+						System.out.println("Laimejo priesininkas, bandykite suzaisti dar karta");
+					}
+	
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
@@ -54,15 +84,10 @@ public class ZaidejasOdeta implements lt.vcs.laivumusis.common.Zaidejas {
 			}
 		}
 
-		@Override
-		public Zaidimas getZaidimas() {
-			return this.zaidimas;
-		}
-
-		public void padekLaivusAntLentos() {
+		/*public void padekLaivusAntLentos() {
 			for (int i = 0; i < zaidimas.duokLaivus("1").size(); i++)
 				zaidimas.pridekLaiva(zaidimas.duokLaivus("1").get(i), "1");
-		}
+		}*/
 
 	}
 
