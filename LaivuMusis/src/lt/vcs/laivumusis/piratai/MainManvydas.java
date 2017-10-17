e8085bfbfc9e1862f09ff695d13a42759e941a18
Geologas <manvydas.saltis@gmail.com>
2017-10-17 20:42:45 +0300

    Updated LaivuValidatorius, added checks in Zaidimas method padekLaiva

---
+++ LaivuMusis/src/lt/vcs/laivumusis/piratai/MainManvydas.java
@@ -0,0 +1,26 @@
+package lt.vcs.laivumusis.piratai;
+
+import lt.vcs.laivumusis.piratai.zaidejas.Zaidejas;
+
+public class MainManvydas {
+
+	public static void main(String[] args) {
+		Zaidimas zaidimas = new Zaidimas();
+		Zaidejas zaid = new Zaidejas(zaidimas);
+		
+		
+		String zaidejoID = zaid.getZaidimas().registruokZaideja();
+		System.out.println(zaid.getZaidimas().registruokZaideja());
+		
+		
+		
+
+		ZaidimoLenta a = new ZaidimoLenta(10, 10);
+		Vaizdas vaizdas = new Vaizdas(a);
+		vaizdas.pieskVaizda();
+		
+		zaidimas.duokZaidimoLenta(zaidejoID);
+
+	}
+
+}
